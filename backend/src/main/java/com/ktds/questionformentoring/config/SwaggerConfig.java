7866abf345c5c089183ec8b674beca42ff65d95f
khyungho yoon <zoomspeed77@gmail.com>
2021-12-16 16:03:46 +0900

    [Fix] 디렉터리 구조 변경에 따른 Swagger ant pattern 변경

--- backend/src/main/java/com/ktds/questionformentoring/config/SwaggerConfig.java
+++ backend/src/main/java/com/ktds/questionformentoring/config/SwaggerConfig.java
@@ -60,8 +60,8 @@ public class SwaggerConfig {
                 .useDefaultResponseMessages(false)
                 .consumes(getConsumeContentTypes())
                 .produces(getProduceContentTypes()).apiInfo(getApiInfo()).select()
-                .apis(RequestHandlerSelectors.basePackage("com.ktds.questionformentoring.controller"))
-                .paths(PathSelectors.ant("/**/*")).build();
+                .apis(RequestHandlerSelectors.basePackage("com.ktds.questionformentoring"))
+                .paths(PathSelectors.ant("/**/**/**/*")).build();
     }
 
     /**
