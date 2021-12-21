d6a77d6d39e0baeb5bfb3046a708e3842212f83c
nueli <asdfz888@naver.com>
2021-12-21 13:09:08 +0900

    [Feat]: 멤버타입으로 게시

---
+++ backend/src/frontend/src/views/admin/Tables/Table.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-card>
+    <v-card-title>
+      {{ title }}
+      <v-spacer></v-spacer>
+      <v-text-field
+        class="search-bar"
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="두 글자 이상 검색하세요"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="items"
+      :search="search"
+    >
+      <template v-slot:delete>
+        <v-btn color="error" @click="deleteMbr">
+          삭제
+        </v-btn>
+      </template>
+    </v-data-table>
+  </v-card>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  props: {
+    items: Array,
+    headers: Array,
+    title: String,
+  },
+  data() {
+    return {
+      search: "",
+    };
+  },
+  methods: {
+    deleteMbr() {
+
+    }
+  },
+};
+</script>
+<style scoped>
+.v-sheet.v-card {
+  box-shadow: none !important;
+}
+.search-bar {
+  margin-right: 10%;
+}
+</style>
