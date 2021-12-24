cd3af51662e1f88b708cddfe0f6d165adaa6c70d
nueli <asdfz888@naver.com>
2021-12-24 13:55:58 +0900

    [Feat]: 관리자 게시판

--- backend/src/frontend/src/views/Home.vue
+++ backend/src/frontend/src/views/Home.vue
@@ -2,12 +2,6 @@
   <v-app id="inspire">
     <v-system-bar app>
       <v-spacer></v-spacer>
-
-      <v-icon>mdi-square</v-icon>
-
-      <v-icon>mdi-circle</v-icon>
-
-      <v-icon>mdi-triangle</v-icon>
     </v-system-bar>
 
     <v-navigation-drawer
@@ -36,7 +30,44 @@
       </v-list>
     </v-navigation-drawer>
 
+      <!-- 알람 -->
+      <v-card
+        class="d-flex flex-row-reverse"
+        flat
+      >
+        <v-icon color="white">mdi-circle</v-icon>
+        <v-icon color="white">mdi-circle</v-icon>
+        <v-btn
+          class="ma-2"
+          icon
+          @click="expand = !expand"
+        >
+          <v-icon large color="indigo lighten-2">mdi-bell</v-icon>
+          <v-badge 
+            :content="message"
+            :value="message"
+            bordered color="red" 
+            offset-x="8" offset-y="0"
+            ></v-badge>
+        </v-btn>
+        <v-expand-x-transition>
+          <v-card
+            v-show="expand"
+            height="100"
+            width="100"
+            class="mx-auto secondary"
+          >
+            <v-list dense>
+              <v-subheader>REPORTS</v-subheader>
+              
+            </v-list>          
+          </v-card>
+        </v-expand-x-transition>
+      </v-card>
+      <!-- 여까지 -->
+
     <v-main>
+
       <v-container
           class="py-8 px-6"
           fluid
@@ -58,11 +89,9 @@ export default {
   data: () => ({
     drawer: null,
     links: [
-      ['mdi-inbox-arrow-down', 'Inbox'],
-      ['mdi-send', 'Send'],
-      ['mdi-delete', 'Trash'],
-      ['mdi-alert-octagon', 'Spam'],
     ],
+    message: 3,
+    expand: false,
   }),
 }
 </script>
