6166581c838c40a9c8b39cf59b67cb8eebfb1f9a
nueli <asdfz888@naver.com>
2021-12-24 09:11:13 +0900

    [Feat]: 레이아웃 변경 (수정 완)

---
+++ backend/src/frontend/src/views/Home.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-app id="inspire">
+    <v-system-bar app>
+      <v-spacer></v-spacer>
+
+      <v-icon>mdi-square</v-icon>
+
+      <v-icon>mdi-circle</v-icon>
+
+      <v-icon>mdi-triangle</v-icon>
+    </v-system-bar>
+
+    <v-navigation-drawer
+        v-model="drawer"
+        app
+    >
+      <v-sheet
+          color="grey lighten-4"
+          class="pa-4"
+      >
+        <v-avatar
+            class="mb-4"
+            color="grey darken-1"
+            size="64"
+        ></v-avatar>
+
+        <div>john@vuetifyjs.com</div>
+      </v-sheet>
+
+      <v-divider></v-divider>
+
+      <v-list>
+        <v-list-item @click="$router.push({ name: 'BoardView' })">멘토링 게시판</v-list-item>
+        <v-list-item @click="$router.push({ name: 'AdminMemberView' })">회원 관리</v-list-item>
+        <v-list-item @click="$router.push({ name: 'AdminBoardView' })">게시글 관리</v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <v-container
+          class="py-8 px-6"
+          fluid
+      >
+        <v-row>
+          <v-col
+              cols="12"
+          >
+              <router-view></router-view>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: null,
+    links: [
+      ['mdi-inbox-arrow-down', 'Inbox'],
+      ['mdi-send', 'Send'],
+      ['mdi-delete', 'Trash'],
+      ['mdi-alert-octagon', 'Spam'],
+    ],
+  }),
+}
+</script>
