bd6a436f6add4df64ca8da659048ef75772fc9c0
chaeyeonkim0223 <tomybaru108@gmail.com>
2021-12-28 10:33:37 +0900

    [Add] : Spring Schedular Test 추가

--- backend/src/main/java/com/ktds/questionformentoring/QuestionForMentoringApplication.java
+++ backend/src/main/java/com/ktds/questionformentoring/QuestionForMentoringApplication.java
@@ -3,9 +3,11 @@ package com.ktds.questionformentoring;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 //@MapperScan(value = {"com.ktds.questionformentoring.board.mapper"})
 @SpringBootApplication
+@EnableScheduling
 public class QuestionForMentoringApplication {
 
 	public static void main(String[] args) {
