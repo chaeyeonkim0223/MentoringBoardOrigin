b82f618bf52a74216bd8c89cfa4312f7e4c99976
chaeyeonkim0223 <tomybaru108@gmail.com>
2021-12-29 11:21:57 +0900

    [Add] : 통계 DTO 추가

---
+++ backend/src/main/java/com/ktds/questionformentoring/scheduler/entity/OutMemberStatusDTO.java
@@ -0,0 +1,21 @@
+package com.ktds.questionformentoring.scheduler.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Getter
+@Setter
+@ToString
+public class OutMemberStatusDTO {
+
+    /* 집계 일자 */
+    private int totYmd;
+    /* 집계 멘토 탈퇴 숫자 */
+    private int totMtrWhdwlNum;
+    /* 집계 멘티 탈퇴 숫자 */
+    private int totMteWhdwlNum;
+
+
+}
