6a499554c021cb92ae75a29248d32c23a7f814ed
nueli <asdfz888@naver.com>
2022-01-03 08:51:27 +0900

    [Feat]: chartView 임시저장

---
+++ backend/src/frontend/src/views/chart/ChartView.vue
@@ -0,0 +1,20 @@
+<template>
+
+
+</template>
+
+<script>
+import Pie from "vue-chartjs";
+
+export default {
+  extends: Pie,
+  props
+  data: () => ({
+
+  }),
+}
+</script>
+
+<style>
+
+</style>
