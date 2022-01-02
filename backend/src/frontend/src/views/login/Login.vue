c24979df64a8099f06b86cebd33645597bd5fe00
khyoon <zoomspeed77@gmail.com>
2022-01-02 19:02:22 +0900

    [Add] Access Token, Refresh Token 체크Backend 로직 추가

--- backend/src/frontend/src/views/login/Login.vue
+++ backend/src/frontend/src/views/login/Login.vue
@@ -1,62 +1,73 @@
-    <template>
-      <div>
-        <h1>JWT 회원가입 화면 입니다.</h1>
-          <button @click="login('testid1', 'testpwd')">rt22est</button>
-          <form @submit.prevent="formSubmit" method="post">
-            <div>
-              <label for="email">email</label>
-              <input id="email" type="text" v-model="email" />
-            </div>
-            <div>
-              <label for="password">password</label>
-              <input id="password" type="text" v-model="password" />
-            </div>
-            <button type="submit">Register</button>
-          </form>
-      </div>
-    </template>
+<template>
+  <div>
+    <h1>JWT 회원가입 화면 입니다.</h1>
+    <div>
+      <label for="userid">userid</label>
+      <input id="userid" type="text" v-model="userid" />
+    </div>
+    <div>
+      <label for="password">password</label>
+      <input id="password" type="text" v-model="password" />
+    </div>
+    <button @click="login(userid, password)">Login</button>
+    <br/>
+    <button @click="logout()">Logout</button>
+  </div>
+</template>
 
-    <script>
-    import axios from "axios";
-    export default {
-      name: "JwtResiter",
-      components: {},
-      data() {
-        return {
-          email: "",
-          password: "",
-          name: "",
-        }
-      },
-      computed: {},
-      created() {
-        console.log('created test')
-        axios.get("/api/boards").then((res) => {
-          this.items = res.data;
-        });
-      },
-      methods: {
-        async login(username, password) {
-          console.log(username, password)
-          console.log(axios)
-          console.log(this.$route)
+<script>
+import axios from "axios";
+export default {
+  name: "JwtLogin",
+  components: {},
+  data() {
+    return {
+      userid: "",
+      password: "",
+      name: "",
+    };
+  },
+  computed() {
 
-          let params = {
-            'loginId': 'aa',
-            'loginPwd': 'aaa'
+  },
+  created() {
+
+  },
+  methods: {
+    async login(username, password) {
+      console.log(username, password);
+      console.log(this.$route);
+
+      let params = {
+        loginId: username,
+        loginPwd: password,
+      };
+
+      console.log(params);
+      axios
+        .post(`api/v1/auth/login`, params)
+        .then((res) => {
+          console.log(res)
+          if (res.data.code === 200) {
+            alert('login success');
+            console.log(res.headers["jwt-auth-token"]);
+            localStorage.setItem("jwt-auth-token", res.headers["jwt-auth-token"]);
+            console.log(localStorage.getItem('jwt-auth-token'));
+          } else {
+            alert('login fail');
           }
-          console.log(params)
-          axios.post(`/v1/auth/login`, params).then((res) => {
-            console.log(res.headers['jwt-auth-token'])
-            localStorage.setItem('jwt-auth-token', res.headers['jwt-auth-token']);
-            localStorage.getItem('jwt-auth-token')
-          }).catch(function(e){
-            console.log(e)
-          });
-          
-        }
-      },
+        })
+        .catch(function (e) {
+          console.log(e);
+        });
+    },
+    logout() {
+      localStorage.removeItem("jwt-auth-token");
+      alert('logout');
+      console.log(localStorage.getItem('jwt-auth-token'));
     }
-    </script>
+  },
+};
+</script>
 
-    <style scoped></style>
+<style scoped></style>
--- backend/src/main/java/com/ktds/questionformentoring/login/service/LoginService.java
+++ backend/src/main/java/com/ktds/questionformentoring/login/service/LoginService.java
@@ -1,15 +1,20 @@
 package com.ktds.questionformentoring.login.service;
 
+import com.ktds.questionformentoring.common.ResponseMsg;
 import com.ktds.questionformentoring.member.entity.MemberDTO;
+import org.springframework.http.ResponseEntity;
 
+import javax.xml.ws.Response;
 import java.util.List;
 import java.util.Map;
 
 public interface LoginService {
-    public String createToken(MemberDTO memberDto);
+    public String createUserToken(MemberDTO memberDto);
+    public String createRefreshToken(MemberDTO memberDto);
     public Map<String, Object> getInfo(String token) throws Exception;
     public void checkValid(String token);
     public boolean checkValidUser(MemberDTO memberDto);
     public List<MemberDTO> findAll();
     public MemberDTO findOne(MemberDTO memberDTO);
+    public ResponseEntity<Object> checkValidToken(String accessToken, String refreshToken);
 }
