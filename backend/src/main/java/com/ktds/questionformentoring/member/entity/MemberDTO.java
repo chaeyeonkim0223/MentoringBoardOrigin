4f806036b991e9f29dec52c371390101e0444e81
nueli <asdfz888@naver.com>
2022-01-05 12:36:16 +0900

    [Feat]: 일시 timestamp 로 통일

--- backend/src/main/java/com/ktds/questionformentoring/member/entity/MemberDTO.java
+++ backend/src/main/java/com/ktds/questionformentoring/member/entity/MemberDTO.java
@@ -11,17 +11,17 @@ import java.util.Date;
 @Builder
 public class MemberDTO {
 
-    private int mbrNo;         // 회원번호
+    private int mbrNo;          // 회원번호
     private String mbrNm;       // 회원명
     private String loginId;     // 로그인 아이디
     private String loginPwd;    // 로그인 비밀번호
     private String mbrTypeCd;   // 회원유형코드
     private String mtrId;       // 멘토아이디
     private String telno;       // 전화번호
-    private Date mbrSbscDt;     // 회원가입일시
-    private Date rtrmmbDt;      // 탈퇴일시
+    private String mbrSbscDt;   // 회원가입일시
+    private String rtrmmbDt;    // 탈퇴일시
     private String rtrmmbWhyCd; // 탈퇴사유코드
-    private Date lastLoginDt;   // 최종 로그인 일시
-    private Date pwdChgDate;    // 비밀번호 변경일자
-    private Date infoAmdDt;     // 정보 수정일자
+    private String lastLoginDt; // 최종 로그인 일시
+    private String pwdChgDate;  // 비밀번호 변경일자
+    private String infoAmdDt;   // 정보 수정일자
 }
--- backend/src/main/java/com/ktds/questionformentoring/member/entity/MemberHistoryDTO.java
+++ backend/src/main/java/com/ktds/questionformentoring/member/entity/MemberHistoryDTO.java
@@ -11,17 +11,19 @@ import java.util.Date;
 @Builder
 public class MemberHistoryDTO {
 
-    private int mbrNo;          // 회원번호
-    private Date hstChgDt;      // 이력 변경 일시
-    private String mbrNm;       // 회원 명
-    private String loginId;     // 로그인 아이디
-    private String mbrTypeCd;   // 회원 유형 코드
-    private String mtrId;       // 멘토아이디
-    private String telno;       // 전화번호
-    private Date mbrSbscDt;     // 회원 가입 일시
-    private Date rtrmmbDt;      // 탈퇴 일시
-    private String rtrmmbWhyCd; // 탈퇴 사유 코드
-    private Date lastLoginDt;   // 최종 로그인 일시
-    private Date pwdChgDate;    // 비밀번호 변경 일자
-    private Date infoAmdDate;   // 정보 수정 일자
+    private int mbrNo;              // 회원번호
+    private String hstChgDt;        // 이력 변경 일시
+    private String mbrNm;           // 회원 명
+    private String loginId;         // 로그인 아이디
+    private String mbrTypeCd;       // 회원 유형 코드
+    private String mtrId;           // 멘토아이디
+    private String telno;           // 전화번호
+    private String mbrSbscDt;       // 회원 가입 일시
+    private String rtrmmbDt;        // 탈퇴 일시
+    private String rtrmmbWhyCd;     // 탈퇴 사유 코드
+    private String lastLoginDt;     // 최종 로그인 일시
+    private String pwdChgDate;      // 비밀번호 변경 일자
+    private String infoAmdDate;     // 정보 수정 일자
+
+    private String mtrNm;       // 멘토 명
 }
